9dc8868b3db2d74a0257802ef2a01b9c26cef8f9
mosa beryal <beryalmosa@gmail.com>
2024-07-17 00:53:42 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App: React.FC = () => {
+  return <div className="bg-red-500">
+  Yes it's working perfect
+  <Button>
+    Hi Boy!
+  </Button>
+  </div>;
+};
+
+export default App;

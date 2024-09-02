ab14118529244b306ccc90f9e639498b64815a53
mosa beryal <beryalmosa@gmail.com>
2024-09-02 18:16:42 +0500

    typescript types added

---
+++ src/types/formTypes.ts
@@ -0,0 +1,5 @@
+import { LoginFormInputs } from "../schemas/authSchema";
+import { RegistrationFormInputs } from "../schemas/authSchema";
+
+export type { RegistrationFormInputs };
+export type { LoginFormInputs };

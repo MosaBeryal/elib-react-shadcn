f5232683704e7d3f0d6db7cecf98c2403e9820a2
mosa beryal <beryalmosa@gmail.com>
2024-09-02 18:17:24 +0500

    Login form state management

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,42 +10,68 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { loginSchema } from "../schemas/authSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginFormInputs } from "../schemas/authSchema";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit: SubmitHandler<LoginFormInputs> = (data) => {
+    console.log("Data submitted:", data);
+  };
+
   return (
     <section className="flex justify-center items-center min-h-screen">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter className="flex flex-col items-end">
-          <Button className="w-full">Sign in</Button>
-          <div className="">
-            Don't have an account?{" "}
-            <Link to="/registration" className="underline">
-              Register
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                {...register("email")}
+                placeholder="m@example.com"
+              />
+              {errors.email && (
+                <span className="text-red-600">{errors.email.message}</span>
+              )}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" {...register("password")} />
+              {errors.password && (
+                <span className="text-red-600">{errors.password.message}</span>
+              )}
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col items-end">
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+            <div className="mt-2">
+              Don't have an account?{" "}
+              <Link to="/registration" className="underline">
+                Register
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      </form>
     </section>
   );
 }
